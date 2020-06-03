0a4364f588c7925b89357314a1c377bbb0c572ac
suwijong <suwijong1317@163.com>
2020-06-03 22:45:54 +0800

    login去换行

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -153,8 +153,6 @@ import {sendCode, loginWithUsername, loginWithPhone} from '../../api'
       }
      },
 
-  
-
     computed:{
       rightPhoneNumber(){
          return /^1(3|4|5|6|7|8|9)\d{9}$/.test(this.phone)
